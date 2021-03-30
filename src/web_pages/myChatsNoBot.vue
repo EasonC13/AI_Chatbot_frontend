869db50e525a5e6063dc103cc267f75566c1117a
EasonC13 <gtes9950406@gmail.com>
2021-03-30 19:48:21 +0800

    Add no bot's condition

---
+++ src/web_pages/myChatsNoBot.vue
@@ -0,0 +1,30 @@
+<template lang="">
+     <div class="col text-center">
+         <p class="h1">You don't have any bot yet...</p>
+        <button type="button" class='btn btn-success'
+         @click="create_bot"><p class="display-4">Create Your First Bot</p></button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'myChatsNoBot',
+    bodyClass: 'myChatsNoBot',
+    components: {
+    },
+    mounted(){
+        
+    },
+    data() {
+      return {
+      }
+    },
+    methods: {
+        create_bot: function(){
+            console.log("QQQQ")
+        }
+    }
+}
+</script>
+<style>
+    
+</style>
