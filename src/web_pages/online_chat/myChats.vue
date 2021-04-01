638891c5e5e49d274b95f780c0e97dd73962f6b2
EasonC13 <gtes9950406@gmail.com>
2021-04-01 14:13:21 +0800

    mychat add emotion mode config

--- src/web_pages/online_chat/myChats.vue
+++ src/web_pages/online_chat/myChats.vue
@@ -14,12 +14,13 @@
                                             <th scope="col">Picture</th>
                                             <th scope="col">Display Name</th>
                                             <!-- <th scope="col">Username</th> -->
+                                            <th scope="col">Mode</th>
                                             <th scope="col">Handle</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr v-for="(item, index) in bots_list" :key="index">
-                                                
+                                            <tr v-for="(item, index) in get_all_bots" :key="index"
+                                             :class="{'bg-light': item.is_new_bot}">
                                                 <th scope="row">
                                                     <img class="profile_img" 
                                                     :src="item['picture_url']" 
@@ -28,10 +29,23 @@
                                                 <td>{{item["display_name"]}}</td>
                                                 <!-- <td>{{item["tg_username"]}}</td> -->
                                                 <td>
+                                                    <div class="dropright" id="online_mychats_select_emotion_menu">
+                                                        <button class="btn dropdown-toggle online_myChat_select_emotion" type="button" 
+                                                         id="dropdownMenuButton" data-toggle="dropdown" 
+                                                         aria-haspopup="true" aria-expanded="false">
+                                                            {{item["emotion"]}}
+                                                        </button>
+                                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                            <a v-for="(emotion, i) in emotions" :key="i"
+                                                             class="dropdown-item" @click="item.emotion = emotion; refreash_both_bot_list()">{{emotion}}</a>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
                                                     <el-tooltip content="More Info"
                                                                 :open-delay="300"
                                                                 placement="top" effect="white">
-                                                        <n-button type="info" size="sm" icon @click.native="Hi">
+                                                        <n-button type="info" size="sm" icon @click.native="">
                                                             <i class="now-ui-icons users_single-02" ></i>
                                                         </n-button>
                                                     </el-tooltip>
@@ -55,44 +69,8 @@
                                                     </el-tooltip>
                                                 </td>
                                             </tr>
-                                            <tr v-for="(item, index) in new_bots_list" :key="'newbot'+index"
-                                             class="bg-light">
-                                                <th scope="row">
-                                                    <img class="profile_img" 
-                                                    :src="item['picture_url']" 
-                                                    alt="Picture" />
-                                                </th>
-                                                <td>{{item["display_name"]}}</td>
-                                                <!-- <td>{{item["tg_username"]}}</td> -->
-                                                <td>
-                                                    <el-tooltip content="More Info"
-                                                                :open-delay="300"
-                                                                placement="top" effect="white">
-                                                        <n-button type="info" size="sm" icon @click.native="Hi">
-                                                            <i class="now-ui-icons users_single-02" ></i>
-                                                        </n-button>
-                                                    </el-tooltip>
-
-
-                                                    <el-tooltip content="Settings"
-                                                                :open-delay="300"
-                                                                placement="top" effect="white">
-                                                        <n-button type="success" size="sm" icon>
-                                                            <i class="now-ui-icons ui-2_settings-90"></i>
-                                                        </n-button>
-                                                    </el-tooltip>
-
-                                                    <el-tooltip content="Remove"
-                                                                :open-delay="300"
-                                                                placement="top" effect="white">
-                                                        <n-button type="danger" size="sm" icon
-                                                         @click.native="remove_bot_from_table_new_bots_list(item)">
-                                                            <i class="now-ui-icons ui-1_simple-remove"></i>
-                                                        </n-button>
-                                                    </el-tooltip>
-                                                </td>
-                                            </tr>
                                             <tr @click="trigger_add_new_bot" v-show="can_add_bot">
+                                                <td>-</td>
                                                 <td>-</td>
                                                 <td>-</td>
                                                 <td>
@@ -116,7 +94,7 @@
                                     @selectBot="add_bot_event" 
                                     @unSelectBot="remove_bot_event"
                                     :already_have_bots="get_bots_username_list"
-                                    :parent_new_bots_list="get_new_bots_list"></addBot>
+                                    :parent_new_bots_list="get_new_bots_list_uid"></addBot>
                                 </b-collapse>
                             </div>
                         </div>
@@ -130,16 +108,17 @@
                 </div>
             </div>
         </div>
-        <div v-else class="container text-center">
+        <div v-if="user_email==undefined" class="container text-center">
             <br>
-            <p class="h2">Please Login To Continue</p>
+            <p class="h2">Loading......</p>
+            <p class="h3">If you not login yet, please Login To Continue</p>
         </div>
     </div>
 </template>
 <script>
 import {Card, Button, Checkbox, Comment, FormGroupInput, Pagination} from '@/components';
 import {Table, TableColumn, Tooltip, Popover} from 'element-ui';
-import { BButton, BCard, BCollapse, VBToggle} from 'bootstrap-vue'
+import { BDropdown, BDropdownItem, BButton, BCard, BCollapse, VBToggle} from 'bootstrap-vue'
 import addBot from "./addBot"
 import myChatsNoBot from "./myChatsNoBot"
 
@@ -153,6 +132,8 @@ export default {
     components: {
         myChatsNoBot,
         addBot,
+        BDropdown,
+        BDropdownItem,
         BButton,
         BCard,
         BCollapse,
@@ -192,6 +173,7 @@ export default {
             new_bots_list: [],
             no_bot: undefined,
             user_email: undefined,
+            emotions: ["😃 Positive", "🤗 Empathic", "🥰 Lovely", "😤 Unfriendly"],
         }
     },
     computed: {
@@ -200,7 +182,7 @@ export default {
             this.bots_list.forEach((el) => out.push(el.picture_url))
             return out
         },
-        get_new_bots_list: function(){
+        get_new_bots_list_uid: function(){
             let out = [];
             this.new_bots_list.forEach((el) => out.push(el.picture_url))
             return out
@@ -209,6 +191,9 @@ export default {
             let total_len = this.bots_list.length + this.new_bots_list.length
             console.log("total_len", total_len)
             return (total_len <= 4)
+        },
+        get_all_bots: function(){
+            return this.bots_list.concat(this.new_bots_list)
         }
     },
     methods:{
@@ -242,6 +227,8 @@ export default {
         },
         add_bot_event: function(item){
             console.log("ADD BOT EVENT " , item)
+            item["emotion"] = this.emotions[0]
+            item["is_new_bot"] = true
             this.new_bots_list.push(item)
         },
         remove_bot_event: function(item){
@@ -254,28 +241,37 @@ export default {
             this.bots_list = this.bots_list.filter(function(obj){
                 return obj["picture_url"] != item["picture_url"]
             })
-        },
-        remove_bot_from_table_new_bots_list: function(item){
             this.new_bots_list = this.new_bots_list.filter(function(obj){
                 return obj["picture_url"] != item["picture_url"]
             })
         },
-        save_changes_click: function(){
-            let out = this.new_bots_list
-            out = out.concat(this.bots_list)
+        save_changes_click: function(reload = true){
+            let out = this.bots_list.concat(this.new_bots_list)
+            out.forEach(x => x.is_new_bot = false)
+            console.log(out)
             Cookies.set("bots", out, { expires: 777 })
+            if(reload){
+                location.reload()
+            }
         },
         cancel: function(){
-            window.reload()
+            location.reload()
         },
         save_changes_and_enter_chatroom: function(){
-            this.save_changes_click()
+            this.save_changes_click(false)
             let bots_str = Cookies.get("bots")
             let bots_list = JSON.parse(bots_str)
             if(bots_list.length>0){
                 window.open(window.location.origin+"/#/online/chatroom/")
             }
             
+        },
+        get_emotion(item){
+            return item.emotion
+        },
+        refreash_both_bot_list(){
+            this.bots_list = this.bots_list.filter(x => true)
+            this.new_bots_list = this.new_bots_list.filter(x => true)
         }
     }
 }
@@ -290,6 +286,14 @@ export default {
 }
 #myChats_table{
     margin-bottom: 0px !important;
+    overflow-y: hidden !important; 
+}
+#online_mychats_select_emotion_menu > .dropdown-menu::before{
+    display: none;
+}
+.online_myChat_select_emotion{
+    background-color: transparent !important;
+    color: black !important;
 }
 </style>
 
